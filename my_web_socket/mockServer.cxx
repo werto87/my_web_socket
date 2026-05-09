a87d8da6c597e283ecc7305988fd043bc2f2d3aa
Waldemar Schneider <huhulasmichinruhe@web.de>
2026-05-10 01:17:33 +0200

    added try catch for debugging

--- my_web_socket/mockServer.cxx
+++ my_web_socket/mockServer.cxx
@@ -69,92 +69,99 @@ MockServer<T>::listener (boost::asio::ip::tcp::endpoint endpoint, std::string lo
   using namespace boost::asio;
   using boost::asio::ip::tcp;
   using tcp_acceptor = use_awaitable_t<>::as_default_on_t<tcp::acceptor>;
-  auto executor = co_await this_coro::executor;
-  acceptor = std::make_unique<tcp_acceptor> (executor, endpoint);
-  while (running.load (std::memory_order_acquire))
+  try
     {
-      try
+      auto executor = co_await this_coro::executor;
+      acceptor = std::make_unique<tcp_acceptor> (executor, endpoint);
+      while (running.load (std::memory_order_acquire))
         {
-          using namespace boost::asio::experimental::awaitable_operators;
-          if (not serverStarted)
+          try
             {
-              {
-                std::lock_guard<std::mutex> lk{ waitForServerStarted };
-                serverStarted = true; // Set 'serverStarted' to true before notifying.
-              }
-              waitForServerStartedCond.notify_all ();
-            }
-          auto socket = co_await acceptor->async_accept ();
-          if constexpr (std::same_as<T, WebSocket>)
-            {
-              auto webSocket = T{ std::move (socket) };
-              webSocket.set_option (websocket::stream_base::timeout::suggested (role_type::server));
-              webSocket.set_option (websocket::stream_base::decorator ([] (websocket::response_type &res) { res.set (http::field::server, std::string (BOOST_BEAST_VERSION_STRING) + " webSocket-server-async"); }));
-              co_await webSocket.async_accept ();
-              webSockets.emplace_back (std::make_shared<MyWebSocket<WebSocket> > (std::move (webSocket), loggingName_, id_));
-            }
-          else if constexpr (std::same_as<T, SSLWebSocket>)
-            {
-              auto webSocket = T{ std::move (socket), *sslContext };
-              webSocket.set_option (websocket::stream_base::timeout::suggested (role_type::server));
-              webSocket.set_option (websocket::stream_base::decorator ([] (websocket::response_type &res) { res.set (http::field::server, std::string (BOOST_BEAST_VERSION_STRING) + " websocket-server-async"); }));
-              co_await webSocket.next_layer ().async_handshake (ssl::stream_base::server, use_awaitable);
-              co_await webSocket.async_accept (use_awaitable);
-              webSockets.emplace_back (std::make_shared<MyWebSocket<SSLWebSocket> > (std::move (webSocket), loggingName_, id_));
-            }
-          auto webSocketItr = std::prev (webSockets.end ());
-          coSpawnTraced (executor,
-                         (*webSocketItr)
-                                 ->readLoop (
-                                     [this, &_webSockets = webSockets, webSocketItr, &_mockServerOption = mockServerOption, &_ioContext = ioContext] (std::string msg) mutable
-                                       {
-                                         for (auto const &[startsWith, callback] : _mockServerOption.callOnMessageStartsWith)
+              using namespace boost::asio::experimental::awaitable_operators;
+              if (not serverStarted)
+                {
+                  {
+                    std::lock_guard<std::mutex> lk{ waitForServerStarted };
+                    serverStarted = true; // Set 'serverStarted' to true before notifying.
+                  }
+                  waitForServerStartedCond.notify_all ();
+                }
+              auto socket = co_await acceptor->async_accept ();
+              if constexpr (std::same_as<T, WebSocket>)
+                {
+                  auto webSocket = T{ std::move (socket) };
+                  webSocket.set_option (websocket::stream_base::timeout::suggested (role_type::server));
+                  webSocket.set_option (websocket::stream_base::decorator ([] (websocket::response_type &res) { res.set (http::field::server, std::string (BOOST_BEAST_VERSION_STRING) + " webSocket-server-async"); }));
+                  co_await webSocket.async_accept ();
+                  webSockets.emplace_back (std::make_shared<MyWebSocket<WebSocket> > (std::move (webSocket), loggingName_, id_));
+                }
+              else if constexpr (std::same_as<T, SSLWebSocket>)
+                {
+                  auto webSocket = T{ std::move (socket), *sslContext };
+                  webSocket.set_option (websocket::stream_base::timeout::suggested (role_type::server));
+                  webSocket.set_option (websocket::stream_base::decorator ([] (websocket::response_type &res) { res.set (http::field::server, std::string (BOOST_BEAST_VERSION_STRING) + " websocket-server-async"); }));
+                  co_await webSocket.next_layer ().async_handshake (ssl::stream_base::server, use_awaitable);
+                  co_await webSocket.async_accept (use_awaitable);
+                  webSockets.emplace_back (std::make_shared<MyWebSocket<SSLWebSocket> > (std::move (webSocket), loggingName_, id_));
+                }
+              auto webSocketItr = std::prev (webSockets.end ());
+              coSpawnTraced (executor,
+                             (*webSocketItr)
+                                     ->readLoop (
+                                         [this, &_webSockets = webSockets, webSocketItr, &_mockServerOption = mockServerOption, &_ioContext = ioContext] (std::string msg) mutable
                                            {
-                                             if (boost::starts_with (msg, startsWith))
-                                               {
-                                                 callback ();
-                                                 break;
-                                               }
-                                           }
-                                         if (_mockServerOption.shutDownServerOnMessage && _mockServerOption.shutDownServerOnMessage.value () == msg)
-                                           {
-                                             coSpawnTraced (ioContext, asyncShutDown (), "MockServer shutDownServerOnMessage asyncShutDown");
-                                           }
-                                         else if (_mockServerOption.closeConnectionOnMessage && _mockServerOption.closeConnectionOnMessage.value () == msg)
-                                           {
-                                             coSpawnTraced (ioContext, (*webSocketItr)->asyncClose (), "MockServer closeConnectionOnMessage asyncClose");
-                                           }
-                                         else if (_mockServerOption.requestResponse.count (msg))
-                                           (*webSocketItr)->queueMessage (_mockServerOption.requestResponse.at (msg));
-                                         else if (not _mockServerOption.requestStartsWithResponse.empty ())
-                                           {
-                                             auto msgFound = false;
-                                             for (auto const &[startsWith, response] : _mockServerOption.requestStartsWithResponse)
+                                             for (auto const &[startsWith, callback] : _mockServerOption.callOnMessageStartsWith)
                                                {
                                                  if (boost::starts_with (msg, startsWith))
                                                    {
-                                                     msgFound = true;
-                                                     (*webSocketItr)->queueMessage (response);
+                                                     callback ();
                                                      break;
                                                    }
                                                }
-                                             if (not msgFound)
+                                             if (_mockServerOption.shutDownServerOnMessage && _mockServerOption.shutDownServerOnMessage.value () == msg)
                                                {
-                                                 spdlog::info ("unhandled message: {}", msg);
+                                                 coSpawnTraced (ioContext, asyncShutDown (), "MockServer shutDownServerOnMessage asyncShutDown");
                                                }
-                                           }
-                                       })
-                             && (*webSocketItr)->writeLoop (),
-                         "MockServer read and write", [&_webSockets = webSockets, webSocketItr] (auto eptr) { _webSockets.erase (webSocketItr); });
-          if (mockServerOption.mockServerRunTime)
+                                             else if (_mockServerOption.closeConnectionOnMessage && _mockServerOption.closeConnectionOnMessage.value () == msg)
+                                               {
+                                                 coSpawnTraced (ioContext, (*webSocketItr)->asyncClose (), "MockServer closeConnectionOnMessage asyncClose");
+                                               }
+                                             else if (_mockServerOption.requestResponse.count (msg))
+                                               (*webSocketItr)->queueMessage (_mockServerOption.requestResponse.at (msg));
+                                             else if (not _mockServerOption.requestStartsWithResponse.empty ())
+                                               {
+                                                 auto msgFound = false;
+                                                 for (auto const &[startsWith, response] : _mockServerOption.requestStartsWithResponse)
+                                                   {
+                                                     if (boost::starts_with (msg, startsWith))
+                                                       {
+                                                         msgFound = true;
+                                                         (*webSocketItr)->queueMessage (response);
+                                                         break;
+                                                       }
+                                                   }
+                                                 if (not msgFound)
+                                                   {
+                                                     spdlog::info ("unhandled message: {}", msg);
+                                                   }
+                                               }
+                                           })
+                                 && (*webSocketItr)->writeLoop (),
+                             "MockServer read and write", [&_webSockets = webSockets, webSocketItr] (auto eptr) { _webSockets.erase (webSocketItr); });
+              if (mockServerOption.mockServerRunTime)
+                {
+                  coSpawnTraced (ioContext, serverShutDownTime (), "serverShutDownTime");
+                }
+            }
+          catch (std::exception const &e)
             {
-              coSpawnTraced (ioContext, serverShutDownTime (), "serverShutDownTime");
+              throw e;
             }
         }
-      catch (std::exception const &e)
-        {
-          throw e;
-        }
+    }
+  catch (std::exception ex)
+    {
+      throw ex;
     }
 }
 template <class T>
